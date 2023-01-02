8d476c64860401af1380b1c485170998fd017a0e
Jaylenwilson <88348326+Jaylenwilson@users.noreply.github.com>
2023-01-02 15:50:05 -0500

    started working on filters layout located in the sidebar

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -7,8 +7,14 @@ import { RiShoppingCartFill } from 'react-icons/ri';
 import { MdFavorite } from 'react-icons/md';
 import { GoPackage } from 'react-icons/go';
 import { BsFilterCircle, BsSearch } from 'react-icons/bs';
+import { Container, Row, Col } from 'reactstrap';
 
 export default function Sidebar(props) {
+
+    // TODO
+    // 1. create animation that transitions from toolbar to search input
+    // 2. add ternary logic that displays which ever tool is selected
+    // 3. create style that shows when a tool in toolbar is active
 
     const [wishListActive, setWishListActive] = useState(false);
     const [shoppingCartActive, setShoppingCartActive] = useState(false);
@@ -38,9 +44,100 @@ export default function Sidebar(props) {
                         </ButtonGroup>
                     </OffcanvasHeader >
                     <OffcanvasBody>
-                        <strong>
-                            This is the Offcanvas body.
-                    </strong>
+                        {filtersActive ?
+                            <Container fluid>
+                                <Row className="justify-content-center d-flex align-items-center my-5">
+                                    <Col xs={4} className="justify-content-center d-flex align-items-center" sm={4}>
+                                        <Button size="large">Men</Button>
+                                    </Col>
+                                    <Col xs={4} className="justify-content-center d-flex align-items-center" sm={4}>
+                                        <Button size="large">Women</Button>
+                                    </Col>
+                                    <Col xs={4} className="justify-content-center d-flex align-items-center" sm={4}>
+                                        <Button size="large">Kids</Button>
+                                    </Col>
+                                </Row>
+                                <h5>Sizes/US:</h5>
+                                <Row className="my-3">
+                                    <Col xs={2}>
+                                        <Button size="small">4</Button>
+                                    </Col>
+                                    <Col xs={2}>
+                                        <Button size="small">4.5</Button>
+                                    </Col>
+                                    <Col xs={2}>
+                                        <Button size="small">5</Button>
+                                    </Col>
+                                    <Col xs={2}>
+                                        <Button size="small">6</Button>
+                                    </Col>
+                                    <Col xs={2}>
+                                        <Button size="small">6.5</Button>
+                                    </Col>
+                                    <Col xs={2}>
+                                        <Button size="small">7</Button>
+                                    </Col>
+                                </Row>
+                                <Row className="my-3">
+                                    <Col xs={2}>
+                                        <Button size="small">7.5</Button>
+                                    </Col>
+                                    <Col xs={2}>
+                                        <Button size="small">8</Button>
+                                    </Col>
+                                    <Col xs={2}>
+                                        <Button size="small">8.5</Button>
+                                    </Col>
+                                    <Col xs={2}>
+                                        <Button size="small">9</Button>
+                                    </Col>
+                                    <Col xs={2}>
+                                        <Button size="small">9.5</Button>
+                                    </Col>
+                                </Row>
+                                <Row className="my-3">
+                                    <Col xs={2}>
+                                        <Button size="small">10</Button>
+                                    </Col>
+                                    <Col xs={2}>
+                                        <Button size="small">10.5</Button>
+                                    </Col>
+                                    <Col xs={2}>
+                                        <Button size="small">11</Button>
+                                    </Col>
+                                    <Col xs={2}>
+                                        <Button size="small">11.5</Button>
+                                    </Col>
+                                    <Col xs={2}>
+                                        <Button size="small">12</Button>
+                                    </Col>
+                                </Row>
+                                <Row className="my-3">
+                                    <Col xs={2}>
+                                        <Button size="small">12.5</Button>
+                                    </Col>
+                                    <Col xs={2}>
+                                        <Button size="small">13</Button>
+                                    </Col>
+                                    <Col xs={2}>
+                                        <Button size="small">14</Button>
+                                    </Col>
+                                    <Col xs={2}>
+                                        <Button size="small">15</Button>
+                                    </Col>
+                                </Row>
+                                <h5>Prices:</h5>
+                                <Row className="my-4">
+                                    <Col xs={6}>
+                                        <Button>Lowest - Highest</Button>
+                                    </Col>
+                                    <Col xs={6}>
+                                        <Button>Highest - Lowest</Button>
+                                    </Col>
+
+                                </Row>
+                            </Container>
+                            : null}
                     </OffcanvasBody>
                 </Offcanvas>
             </div>
