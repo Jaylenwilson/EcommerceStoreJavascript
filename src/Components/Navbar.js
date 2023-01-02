2860018c9e2876b4ff586c3180005997fb21955d
Jaylenwilson <88348326+Jaylenwilson@users.noreply.github.com>
2023-01-02 14:09:18 -0500

    created toolbar for sidebar where a user will be able to switch between shopping cart, favorites, orders, and filters.

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Row, Col, } from 'reactstrap';
 import { useState } from 'react';
 import Sidebar from './Sidebar';
 export default function Navbar() {
+
+
     const [show, setShow] = useState(false)
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(false)
+    const handleClose = () => {
+        setShow(false)
+        console.log(show)
+    };
+    const handleShow = () => {
+        setShow(true)
+        console.log(show)
+    }
+
+
+
+
     return (
         <>
             <nav className="header">
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,31 +1,42 @@
-import React, { useState } from 'react'
-import { Button, Offcanvas, OffcanvasHeader, OffcanvasBody, } from 'reactstrap';
-
+import React, { useState } from 'react';
+import { Button, Offcanvas, OffcanvasHeader, OffcanvasBody, ButtonGroup } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faBarsProgress } from '@fortawesome/free-solid-svg-icons';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { RiShoppingCartFill } from 'react-icons/ri';
+import { MdFavorite } from 'react-icons/md';
+import { GoPackage } from 'react-icons/go';
+import { BsFilterCircle, BsSearch } from 'react-icons/bs';
 
 export default function Sidebar(props) {
-    const [show, setShow] = useState(false)
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(false)
 
-
-
-
+    const [wishListActive, setWishListActive] = useState(false);
+    const [shoppingCartActive, setShoppingCartActive] = useState(false);
+    const [ordersActive, setOrdersActive] = useState(false);
+    const [filtersActive, setFiltersActive] = useState(true);
 
 
     return (
         <div>
             <div>
-                <Button
+                <Button size='md'
                     color="primary"
-                    onClick={handleShow}
+                    onClick={props.handleShow}
                 >
-                    Open
+
+                    <GiHamburgerMenu size='32px' />
                 </Button>
-                <Offcanvas show={show}
-                    onhide={handleClose}                >
+                <Offcanvas show={props.show}
+                    onHide={props.handleClose}                >
                     <OffcanvasHeader closeButton>
-                        Offcanvas
-                </OffcanvasHeader >
+                        <ButtonGroup size='md' aria-label="Basic example">
+                            <Button className="shoppingcart" variant="secondary"><RiShoppingCartFill size='20px' title="Shopping Cart" /></Button>
+                            <Button className="favoriteicon" variant="secondary"><MdFavorite size='20px' color="white" title="favorites" /></Button>
+                            <Button variant="secondary"><GoPackage size='20px' title="Shoebox/Orders" /></Button>
+                            <Button variant="secondary"><BsFilterCircle size='20px' title="Filters" /></Button>
+                            <Button variant="secondary"><BsSearch size='20px' title="Search" /></Button>
+                        </ButtonGroup>
+                    </OffcanvasHeader >
                     <OffcanvasBody>
                         <strong>
                             This is the Offcanvas body.
